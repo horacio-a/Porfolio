937db9a57549ba4489c95911bc365240b1320f3d
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-08-28 16:22:20 -0300

    <feat> [pages\404.js]: error 404 pages

---
+++ pages/404.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Custom404() {
+
+
+    return (
+        <>
+            <Head >
+                <link rel="icon" href="\imagenes\main\darkS.png" />
+
+                <meta name="description" content="Porfolio de horacio Albornoz" />
+                <title>Elegí un estilo</title>
+            </Head>
+            <main className={`errorPage`}>
+                <div className="conteinerImg">
+                    <Link className="imgLogo" href={'/'}>
+                        <img src="/imagenes/main/whiteL.png" />
+                    </Link>
+                </div>
+                <div className="conteinerTxt">
+                    <div className="BigTitle">EEHH...</div>
+                    <div className="SmallTitle">Creo que no tendrías que estar aquí</div>
+                    <a href="/" className="bntBack">
+                        Volver
+                    </a>
+                </div>
+
+            </main>
+        </>
+    )
+}
