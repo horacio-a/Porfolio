2b95fabd09f39d19a764a0ffb2335c69d5d56c9d
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-09-06 16:07:46 -0300

    <feat> [componets\animationScrollAboutMe.js]: animation AboutMe

---
+++ componets/animationScrollAboutMe.js
@@ -0,0 +1,88 @@
+const AnimationScrollAboutMe = ({ animationNumaboutMe }) => {
+    return (
+        <div className="AnimationScrollTitles" style={{ transform: `translateX(-${animationNumaboutMe}%)` }}>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH  -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+            <div className="textAnimation">SOBRE MI -<>&nbsp;</></div>
+            <div className="textAnimation">나 소개 -<>&nbsp;</></div>
+            <div className="textAnimation">UBER MICH -<>&nbsp;</></div>
+            <div className="textAnimation">ABOUT ME -<>&nbsp;</></div>
+            <div className="textAnimation">SU DI ME -<>&nbsp;</></div>
+        </div>
+    )
+}
+
+export default AnimationScrollAboutMe
