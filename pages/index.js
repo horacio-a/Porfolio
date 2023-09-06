3a75e15526f27608525bd0e3382b33e10b87483e
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-09-06 16:16:53 -0300

    <feat> [pages\index.js]: change style for porfolio

--- pages/index.js
+++ pages/index.js
@@ -1,111 +1,167 @@
 import React, { useEffect, useRef, useState } from "react";
-import { scroller } from "react-scroll";
-import HeaderMain from "../componets/headermain";
-import { Waypoint } from 'react-waypoint';
 import Head from "next/head";
-import Hero from "@/componets/main/hero";
-import Project from "@/componets/main/project";
-import Hability from "@/componets/main/hability";
-import Contact from "@/componets/main/contact";
-import MainFooter from "@/componets/main/MainFooter";
-import { Toaster } from "react-hot-toast";
-
+import AnimationScrollDev from "@/componets/animaationScrollDev";
+import AnimationScrollProject from "@/componets/animationScrollProject";
+import AnimationScrollAboutMe from "@/componets/animationScrollAboutMe";
+import AnimationScrollContact from "@/componets/animationScrollContact";
+import ProjectContent from "@/componets/project";
+import AboutMeContent from "@/componets/AboutMe";
+import ContactContent from "@/componets/contact";
+import { isMobile } from 'react-device-detect';
 
 const Index = () => {
+    console.log(isMobile)
 
     const [TitlePage, setTitlePage] = useState('Horacio Albornoz')
     const [scrollItem, setScrollItem] = useState(0)
 
-    useEffect(() => {
-        ScrolltoInicio()
-    }, [])
+    const [Project, setProject] = useState(false);
+    const [animationNumProject, setanimationNumProject] = useState(0);
+    const [storeIntervalProject, setStoreIntervalProject] = useState();
 
-    const changebyscrotum = (data) => {
-        setScrollItem(data)
-    }
+    const increaseAnimationNumProject = () => {
+        setanimationNumProject((animationNumProject) => animationNumProject + 0.05);
+        if (animationNumProject > 5000) setanimationNumProject((animationNumProject) => 0);
+    };
+
+    const onMouseOverProject = () => {
+        clearInterval(storeIntervalProject);
+        setStoreIntervalProject(setInterval(increaseAnimationNumProject, 1));
+    };
+
+
+    const [aboutMe, setaboutMe] = useState(false);
+    const [animationNumaboutMe, setanimationNumaboutMe] = useState(0);
+    const [storeIntervalaboutMe, setStoreIntervalaboutMe] = useState();
+
+    const increaseAnimationNumaboutMe = () => {
+        setanimationNumaboutMe((animationNumaboutMe) => animationNumaboutMe + 0.05);
+        if (animationNumaboutMe > 5000) setanimationNumaboutMe((animationNumaboutMe) => 0);
+    };
+
+    const onMouseOveraboutMe = () => {
+        clearInterval(storeIntervalaboutMe);
+        setStoreIntervalaboutMe(setInterval(increaseAnimationNumaboutMe, 1));
+    };
 
 
 
-    const ScrolltoProyect = () => {
-        scroller.scrollTo('proyectos', {
-            duration: 500,
-            delay: 50,
-            smooth: true,
-            offset: -50
-        })
-    }
-    const ScrolltoInicio = () => {
-        scroller.scrollTo('inicio', {
-            duration: 500,
-            delay: 50,
-            smooth: true,
-            offset: -50
-        })
-    }
-    const Scrolltohabilidades = () => {
-        scroller.scrollTo('habilidades', {
-            duration: 500,
-            delay: 50,
-            smooth: true,
-            offset: 5
-        })
-    }
-    const ScrolltoContacto = () => {
-        scroller.scrollTo('Contacto', {
-            duration: 500,
-            delay: 50,
-            smooth: true,
-            offset: 0
-        })
-    }
-    const ScrolltoFooter = () => {
-        scroller.scrollTo('Footer', {
-            duration: 500,
-            delay: 50,
-            smooth: true,
-            offset: 5
-        })
-    }
+    const [Contact, setContact] = useState(false);
+    const [animationNumContact, setanimationNumContact] = useState(0);
+    const [storeIntervalContact, setStoreIntervalContact] = useState();
+
+    const increaseAnimationNumContact = () => {
+        setanimationNumContact((animationNumContact) => animationNumContact + 0.05);
+        if (animationNumContact > 5000) setanimationNumContact((animationNumContact) => 0);
+    };
+
+    const onMouseOverContact = () => {
+        clearInterval(storeIntervalContact);
+        setStoreIntervalContact(setInterval(increaseAnimationNumContact, 1));
+    };
 
 
     return (
-        <div className="FondoWhite">
+        <main className="modernBackground">
             <Head>
-                <link rel="icon" href="\imagenes\main\darkS.png" />
+                <link rel="icon" href="Logo.png" />
                 <meta name="description" content="Porfolio de horacio Albornoz" />
                 <title>{TitlePage}</title>
             </Head>
 
 
-            <Waypoint
-                onEnter={() => { changebyscrotum(0); setTitlePage('Horacio Albornoz') }} />
+            <div className="content">
+                <header className="modern" style={Project ? { border: 'none' } : (aboutMe ? { border: 'none' } : (Contact ? { border: 'none' } : {}))}>
+                    HORACIO
+                </header>
+                <div className={`conteinerMainProject ${Project ? 'Open' : ''} ${aboutMe ? 'Desactive' : ''} ${Contact ? 'Desactive' : ''}`}>
+                    <div className={`conteinerProject ${Project ? 'Open' : ''} ${aboutMe ? 'Desactive' : ''} ${Contact ? 'Desactive' : ''}`}
+                        onMouseOver={
+                            () => {
+                                if (!isMobile) {
+                                    Project ? () => { clearInterval(storeIntervalProject); } : onMouseOverProject()
+                                }
+                            }}
+                        onMouseOut={() => {
+                            if (!isMobile) {
+                                if (animationNumProject > 7500) setanimationNumProject((animationNumProject) => 0);
+                                clearInterval(storeIntervalProject);
+                            }
+                        }}
+                        onClick={() => {
+                            !isMobile ? setProject(true) : () => {
+                                setProject(true)
+                                onMouseOverProject()
+                            }
+                        }}
+                    >
+                        <div className="titleProject">
+                            <AnimationScrollProject animationNumProject={animationNumProject} />
+                        </div>
+                    </div>
+                    <div className={`ProjectContent ${Project ? 'Open' : ''}`}>
+                        <div className="btnBack">
+                            <div className="TextBtnBack" onClick={() => {
+                                if (!isMobile) {
+                                    setProject(false)
+                                } else {
+                                    setProject(false)
+                                    clearInterval(storeIntervalProject);
+                                }
+                            }}>VOLVER</div>
+                        </div>
+                        <ProjectContent />
 
-            <HeaderMain Scrolltohabilidades={Scrolltohabilidades} ScrolltoContacto={ScrolltoContacto}
-                ScrolltoFooter={ScrolltoFooter} ScrolltoInicio={ScrolltoInicio} scrollItem={scrollItem}
-                setScrollItem={setScrollItem} ScrolltoProyect={ScrolltoProyect} />
-            <Hero />
+                    </div>
+                </div>
 
+                <div className={`conteinerMainProject ${Project ? 'Desactive' : ''} ${aboutMe ? 'Open' : ''} ${Contact ? 'Desactive' : ''}`}>
 
-            <Project changebyscrotum={changebyscrotum} setTitlePage={setTitlePage} />
+                    <div className={`conteinerProject ${Project ? 'Desactive' : ''} ${aboutMe ? 'Open' : ''} ${Contact ? 'Desactive' : ''}`}>
+                        <div className="titleProject"
+                            onMouseOver={aboutMe ? () => { clearInterval(storeIntervalaboutMe); } : onMouseOveraboutMe}
+                            onMouseOut={() => {
+                                if (animationNumaboutMe > 7500) setanimationNumaboutMe((animationNumaboutMe) => 0);
+                                clearInterval(storeIntervalaboutMe);
+                            }}
+                            onClick={() => { setaboutMe(true) }}
+                        >
+                            <AnimationScrollAboutMe animationNumaboutMe={animationNumaboutMe} />
+                        </div>
+                    </div>
+                    <div className={`ProjectContent ${aboutMe ? 'Open' : ''}`}>
+                        <div className="btnBack">
+                            <div className="TextBtnBack" onClick={() => { setaboutMe(false) }}>VOLVER</div>
+                        </div>
+                        <AboutMeContent />
+                    </div>
+                </div>
+                <div className={`conteinerMainProject ${Project ? 'Desactive' : ''} ${aboutMe ? 'Desactive' : ''} ${Contact ? 'Open' : ''}`}>
 
+                    <div className={`conteinerProject ${Project ? 'Desactive' : ''} ${aboutMe ? 'Desactive' : ''} ${Contact ? 'Open' : ''}`}>
+                        <div className="titleProject"
+                            onMouseOver={Contact ? () => { clearInterval(storeIntervalContact); } : onMouseOverContact}
+                            onMouseOut={() => {
+                                if (animationNumContact > 7500) setanimationNumContact((animationNumContact) => 0);
+                                clearInterval(storeIntervalContact);
+                            }}
+                            onClick={() => { setContact(true) }}>
+                            <AnimationScrollContact animationNumContact={animationNumContact} />
+                        </div>
+                    </div>
+                    <div className={`ProjectContent ${Contact ? 'Open' : ''}`}>
+                        <div className="btnBack">
+                            <div className="TextBtnBack" onClick={() => { setContact(false) }}>VOLVER</div>
+                        </div>
+                        <ContactContent />
+                    </div>
+                </div>
 
-
-            <Hability changebyscrotum={changebyscrotum} setTitlePage={setTitlePage} />
-
-
-
-
-            <Contact changebyscrotum={changebyscrotum} setTitlePage={setTitlePage} />
-
-
-            <MainFooter ScrolltoProyect={ScrolltoProyect} Scrolltohabilidades={Scrolltohabilidades}
-                ScrolltoContacto={ScrolltoContacto} changebyscrotum={changebyscrotum} setTitlePage={setTitlePage} />
-            <Toaster
-                toastOptions={{
-                    className: 'fontGotham'
-                }} />
-
-        </div>
+            </div>
+            <div className="AutoScrollAnimation">
+                <AnimationScrollDev />
+            </div>
+        </main>
 
 
 
