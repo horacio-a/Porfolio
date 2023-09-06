3f5087cd437853dadc5dc01dc02266c8dd905208
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-09-06 16:12:47 -0300

    <feat> [styles]:

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '@/styles/eyes.css'
 import '@/styles/hub.css'
 import '@/styles/space.css'
 import '@/styles/errorPage.css'
+import '@/styles/modernBackground.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
