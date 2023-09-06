7e3e39fc8092028221e23a597b8f956f6903a57f
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-09-06 16:08:48 -0300

    <feat> [componets\animationScrollProject.js]: animation project

---
+++ componets/animationScrollProject.js
@@ -0,0 +1,86 @@
+const AnimationScrollProject = ({ animationNumProject }) => {
+    return (
+        <div className="AnimationScrollTitles" style={{ transform: `translateX(-${animationNumProject}%)` }}>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+            <div className="textAnimation">MY PROJECTS -<>&nbsp;</></div>
+            <div className="textAnimation">OS MEUS PROJECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">I MIEI PROGETTI -<>&nbsp;</></div>
+            <div className="textAnimation">MIS PROYECTOS -<>&nbsp;</></div>
+            <div className="textAnimation">MINE PROJEKTER -<>&nbsp;</></div>
+
+        </div>
+    )
+}
+
+export default AnimationScrollProject
