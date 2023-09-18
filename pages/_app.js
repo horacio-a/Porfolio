9ff1b7f1047c3ed998a9204d4653a9e233435e9e
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-09-18 10:53:59 -0300

    <fix> [pages\_app.js]: useless import delete

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
-import '@/styles/main.css'
-import '@/styles/eyes.css'
-import '@/styles/hub.css'
-import '@/styles/space.css'
+
 import '@/styles/errorPage.css'
 import '@/styles/modernBackground.css'
 
