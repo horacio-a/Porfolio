84c26b1c38be6dd317d3cc7da4d63b2002e6a9a4
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2024-01-17 11:29:27 -0300

    update metadato

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 
 import '@/styles/errorPage.css'
 import '@/styles/modernBackground.css'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import { Analytics } from '@vercel/analytics/react';
 
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
       <Analytics />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,23 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
+      <title>HORACIO DEV - Desarrollador Web Creativo</title>
+      <meta name="title" content="HORACIO DEV - Desarrollador Web Creativo" />
+      <meta name="description" content="Explora el mundo digital a través de mis ojos creativos. Soy Horacio, un apasionado desarrollador web, transformando ideas en experiencias cautivadoras. Descubre la fusión de código y creatividad en cada proyecto " />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.horacio-dev.com.ar/" />
+      <meta property="og:title" content="HORACIO DEV - Desarrollador Web Creativo" />
+      <meta property="og:description" content="Explora el mundo digital a través de mis ojos creativos. Soy Horacio, un apasionado desarrollador web, transformando ideas en experiencias cautivadoras. Descubre la fusión de código y creatividad en cada proyecto " />
+      <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://www.horacio-dev.com.ar/" />
+      <meta property="twitter:title" content="HORACIO DEV - Desarrollador Web Creativo" />
+      <meta property="twitter:description" content="Explora el mundo digital a través de mis ojos creativos. Soy Horacio, un apasionado desarrollador web, transformando ideas en experiencias cautivadoras. Descubre la fusión de código y creatividad en cada proyecto " />
+      <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
       <body data-np-watching="1" data-np-autofill-form-type="other" data-np-checked="1">
         <Main />
         <NextScript />
