78a0990aa65a5d54c6b643e22469e326bcd4e7ac
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2024-01-19 10:29:40 -0300

    <fix> [_documente] link og img

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default function Document() {
       <Head />
 
       <title>HORACIO DEV - Desarrollador Web Creativo</title>
+      <link rel="icon" href="Logo.png" />
+
       <meta name="title" content="HORACIO DEV - Desarrollador Web Creativo" />
       <meta name="description" content="Explora el mundo digital a través de mis ojos creativos. Soy Horacio, un apasionado desarrollador web, transformando ideas en experiencias cautivadoras. Descubre la fusión de código y creatividad en cada proyecto " />
 
@@ -13,13 +15,13 @@ export default function Document() {
       <meta property="og:url" content="https://www.horacio-dev.com.ar/" />
       <meta property="og:title" content="HORACIO DEV - Desarrollador Web Creativo" />
       <meta property="og:description" content="Explora el mundo digital a través de mis ojos creativos. Soy Horacio, un apasionado desarrollador web, transformando ideas en experiencias cautivadoras. Descubre la fusión de código y creatividad en cada proyecto " />
-      <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+      <meta property="og:image" content="https://res-console.cloudinary.com/dioe9ktns/media_explorer_thumbnails/0d7e8482bdbf6fc4575575dba6a22acb/detailed" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://www.horacio-dev.com.ar/" />
       <meta property="twitter:title" content="HORACIO DEV - Desarrollador Web Creativo" />
       <meta property="twitter:description" content="Explora el mundo digital a través de mis ojos creativos. Soy Horacio, un apasionado desarrollador web, transformando ideas en experiencias cautivadoras. Descubre la fusión de código y creatividad en cada proyecto " />
-      <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+      <meta property="twitter:image" content="https://res-console.cloudinary.com/dioe9ktns/media_explorer_thumbnails/0d7e8482bdbf6fc4575575dba6a22acb/detailed" />
 
       <body data-np-watching="1" data-np-autofill-form-type="other" data-np-checked="1">
         <Main />
